d0f66f6e270b6e54faef7dcdf28705f6bba88f9a
anniepates <82011145+anniepates@users.noreply.github.com>
2023-04-07 16:29:02 -0400

    still testing merges

--- FinalProject/graphics_project/source/SDL_Surfaces.d
+++ FinalProject/graphics_project/source/SDL_Surfaces.d
@@ -3,9 +3,13 @@ import std.string;
 import std.math;
 import std.algorithm;
 
-// Load the SDL2 library
+/// Load the SDL2 library
 import bindbc.sdl;
 import loader = bindbc.loader.sharedlib;
+
+/**
+Surface class creates an SDL surface on which to draw and includes drawing methods
+*/
 
 class Surface{
     uint flags;
@@ -18,6 +22,13 @@ class Surface{
     uint Amask;
     SDL_Surface* imgSurface;
 
+    /**
+    Surface Constructor:
+    In: 
+        flags: SLD flags,
+        width: surface width, height: surface height, depth: surface depth
+        Rmask, Gmask, Bmask, Amask: red, green, blue, and alpha mask for pixels
+    */
     this(uint flags, int width, int height, int depth,
         uint Rmask, uint Gmask, uint Bmask, uint Amask) {
         this.flags = flags;
@@ -33,35 +44,45 @@ class Surface{
 
     }
     ~this(){
-        // Free a surface...
+        /// Free a surface...
         scope(exit) {
             SDL_FreeSurface(imgSurface);
         }
     }
 
+    /**
+    Returns created surface
+    */
     SDL_Surface* getSurface() {
         return imgSurface;
     }
 
-
+    /**
+    In:    
+        xPos: x-coordinate of pixel, yPos: y-coordinate of pixel
+        blueVal: rgb blue value, greenVal: rgb green value, redVal: rgb red value
+    Changes pixel color at xPos, yPos
+    */
     void UpdateSurfacePixel(int xPos, int yPos, ubyte blueVal, ubyte greenVal, ubyte redVal){
-        // When we modify pixels, we need to lock the surface first
+        /// When we modify pixels, we need to lock the surface first
         SDL_LockSurface(imgSurface);
-        // Make sure to unlock the surface when we are done.
+        /// Make sure to unlock the surface when we are done.
         scope(exit) SDL_UnlockSurface(imgSurface);
-        // Retrieve the pixel arraay that we want to modify
+        /// Retrieve the pixel arraay that we want to modify
         ubyte* pixelArray = cast(ubyte*)imgSurface.pixels;
-        // Change the 'blue' component of the pixels
+        /// Change the 'blue' component of the pixels
         pixelArray[yPos*imgSurface.pitch + xPos*imgSurface.format.BytesPerPixel+0] = blueVal;
-        // Change the 'green' component of the pixels
+        /// Change the 'green' component of the pixels
         pixelArray[yPos*imgSurface.pitch + xPos*imgSurface.format.BytesPerPixel+1] = greenVal;
-        // Change the 'red' component of the pixels
+        /// Change the 'red' component of the pixels
         pixelArray[yPos*imgSurface.pitch + xPos*imgSurface.format.BytesPerPixel+2] = redVal;
     }
 
-    //int array for fetching the colors of a pixel
+    /// Int array for fetching the colors of a pixel
     int[3] colors;
-    //method for getting the RGB values of the pixel passed in
+    /**
+    Method for getting the RGB values of the pixel passed in
+    */
     int[] PixelAt(int xPos, int yPos){
         ubyte* pixelArray = cast(ubyte*)imgSurface.pixels;
         int index = yPos*imgSurface.pitch + xPos*imgSurface.format.BytesPerPixel;
@@ -73,8 +94,12 @@ class Surface{
 
     }
 
-    /// https://www.redblobgames.com/grids/line-drawing.html
-    void lerp(int x1, int y1, int x2, int y2, int brushSize, ubyte red, ubyte green, ubyte blue) {
+    /**
+
+
+    Reference: https://www.redblobgames.com/grids/line-drawing.html
+    */
+    void lerp(int x1, int y1, int x2, int y2, int brushSize, ubyte redVal, ubyte greenVal, ubyte blueVal) {
         
         int numPoints = getNumPoints(x1, y1, x2, y2);
         for (int i = 0; i <= numPoints; ++i) {
@@ -82,7 +107,7 @@ class Surface{
             if (numPoints > 0) {
                 t = cast(float) i / cast(float) numPoints;
             }
-            drawLerpPoint(x1, y1, x2, y2, t, brushSize, red, green, blue);
+            drawLerpPoint(x1, y1, x2, y2, t, brushSize, redVal, greenVal, blueVal);
         }
     }
 
@@ -90,6 +115,7 @@ class Surface{
         return max(abs(x2 - x1), abs(y2 - y1));
     }
 
+    /// Draws a 
     void drawLerpPoint(int x1, int y1, int x2, int y2, float t, int brushSize, ubyte red, ubyte green, ubyte blue) {
         int x = cast(int) round(lerpHelper(x1, x2, t));
         int y = cast(int) round(lerpHelper(y1, y2, t));
@@ -100,6 +126,7 @@ class Surface{
         }
     }
 
+    /// Creates a brush TODO: verify
     float lerpHelper(int start, int end, float t) {
         return start * (1.0 - t) + end * t;
     }
