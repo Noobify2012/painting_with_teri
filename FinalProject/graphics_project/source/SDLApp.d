19e80a4fc52e5d5720ad8bb7a90e48d0a375100a
anniepates <82011145+anniepates@users.noreply.github.com>
2023-04-08 02:17:24 -0400

    commented SDLApp and SDL_Initial

--- FinalProject/graphics_project/source/SDLApp.d
+++ FinalProject/graphics_project/source/SDLApp.d
@@ -1,46 +1,50 @@
-// Import D standard libraries
+/// Import D standard libraries
 import std.stdio;
 import std.string;
 import std.process;
 import std.conv;
 
-// Load the SDL2 library
+/// Load the SDL2 library
 import bindbc.sdl;
 import loader = bindbc.loader.sharedlib;
 import SDL_Surfaces :Surface;
 import SDL_Initial :SDLInit;
-//#include SDL.h;
+// #include SDL.h;
 import test_client;
 
 import shapes;
 import drawing_utilities;
 
-//For printing the key pressed info
-//void PrintKeyInfo( SDL_KeyboardEvent *key );
+// For printing the key pressed info
+// void PrintKeyInfo( SDL_KeyboardEvent *key );
 
 
-
+/**
+Main application class. 
+Creates SDL screen and performs appropriate paint actions when prompted.
+*/
 class SDLApp{
 
-    // global variable for sdl;
+    /// Global variable for sdl;
     const SDLSupport ret;
 
 
     void MainApplicationLoop(){
-        // Create an SDL window
+        /// Create an SDL window
         SDL_Window* window= SDL_CreateWindow("A Teri Chadbourne Experience",
-        SDL_WINDOWPOS_UNDEFINED,
-        SDL_WINDOWPOS_UNDEFINED,
-        640,
-        480,
-        SDL_WINDOW_SHOWN);
-        // Load the bitmap surface
+            SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED,
+            640,
+            480,
+            SDL_WINDOW_SHOWN);
+        /// Load the bitmap surface
         Surface imgSurface = new Surface(0,640,480,32,0,0,0,0);
 
-        // Flag for determing if we are running the main application loop
+        /// Initialize variables
+        /// Application running flag for determing if we are running the main application loop
         bool runApplication = true;
-        // Flag for determining if we are 'drawing' (i.e. mouse has been pressed
-        //                                                but not yet released)
+        /// Drawing flag for determining if we are 'drawing' (i.e. mouse has been pressed
+        ///                                                but not yet released)
         bool drawing = false;
 
         bool change = false;
@@ -61,7 +65,7 @@ class SDLApp{
 
         DrawingUtility du = new DrawingUtility();
 
-        //SDL_EnableUNICODE( 1 );
+        // SDL_EnableUNICODE( 1 );
 
         // Main application loop that will run until a quit event has occurred.
         // This is the 'main graphics loop'
@@ -98,31 +102,33 @@ class SDLApp{
                         brushSize = 16;
                     }
 
+                    /// Change brush:
                     for(int w=-brushSize; w < brushSize; w++){
                         for(int h=-brushSize; h < brushSize; h++){
-                            //blue
+                            /// Set brush color to blue
                             if (color == 1 && !erasing) {
                                 red = 255;
                                 green = 128;
                                 blue = 32;
-                                //imgSurface.UpdateSurfacePixel(xPos+w,yPos+h, red, green, blue);
+                                // imgSurface.UpdateSurfacePixel(xPos+w,yPos+h, red, green, blue);
                             } else if (color == 2 && !erasing) {
-                                //green
+                                /// Set brush color to green
                                 red = 32;
                                 green = 255;
                                 blue = 128;
-                                //imgSurface.UpdateSurfacePixel(xPos+w,yPos+h, 32, 255, 128);
+                                // imgSurface.UpdateSurfacePixel(xPos+w,yPos+h, 32, 255, 128);
                             } else if (color == 3 && !erasing) {
-                                //red
+                                /// Set brush color to red
                                 red = 128;
                                 green = 32;
                                 blue = 255;
-                                //imgSurface.UpdateSurfacePixel(xPos+w,yPos+h,  128, 32, 255);
+                                // imgSurface.UpdateSurfacePixel(xPos+w,yPos+h,  128, 32, 255);
                             } else if (erasing) {
+                                /// Erase: set color to black
                                 red = 0;
                                 green = 0;
                                 blue = 0;
-                                //imgSurface.UpdateSurfacePixel(xPos+w,yPos+h, 0, 0, 0);
+                                // imgSurface.UpdateSurfacePixel(xPos+w,yPos+h, 0, 0, 0);
                             }
                             imgSurface.UpdateSurfacePixel(xPos+w,yPos+h, red, green, blue);
                             if(networked == true) {
@@ -130,26 +136,28 @@ class SDLApp{
                             }
                         }
                     }
+                    /// This is where we draw the line!
                     if (prevX > -9999) {
                         // imgSurface.linearInterpolation(prevX, prevY, xPos, yPos, brushSize, red, green, blue);
                         imgSurface.lerp(prevX, prevY, xPos, yPos, brushSize, red, green, blue);
                     }
                     prevX = xPos;
                     prevY = yPos;
-                    //if keyboard is pressed check for change event
-                } else if(e.type == SDL_KEYDOWN) {
-                    //writeln()
-                    //PrintKeyInfo( &e.key );
-                    //printf( ", Name: %s", SDL_GetKeyName( key.keysym.sym ) );
+                    /// If keyboard is pressed check for change event
+                } else if(e.type == SDL_KEYDOWN) { /// TODO: What is happening here? 
+                    // writeln()
+                    // PrintKeyInfo( &e.key );
+                    // printf( ", Name: %s", SDL_GetKeyName( key.keysym.sym ) );
                     if (e.key.keysym.sym == SDLK_b) {
-                        //printf("Changing brush size");
+                        // printf("Changing brush size");
 
                     }
-                    //printf( cast(string)(e.key.keysym.sym) , " key pressed ");
-                    //printf( SDL_GetKeyNamse (e.key.keysym.sym ) , " key pressed ");
+                    // printf( cast(string)(e.key.keysym.sym) , " key pressed ");
+                    // printf( SDL_GetKeyNamse (e.key.keysym.sym ) , " key pressed ");
 
 
                 } else if(e.type == SDL_KEYUP) {
+                    /// Change brush size
                     printf("key released: ");
                     //, to!string(e.key.keysym.sym));
                     if (e.key.keysym.sym == SDLK_b) {
@@ -160,6 +168,7 @@ class SDLApp{
                         }
                         writeln("Changing to brush size: " , to!string(brush));
                     } else if (e.key.keysym.sym == SDLK_c) {
+                        /// Change brush color (rotate through preset colors at each press)
                         if (color < 3) {
                             color++;
                         } else {
@@ -187,6 +196,7 @@ class SDLApp{
                         }
                         
                     } else if (e.key.keysym.sym == SDLK_f) {
+                        /// This is where we fill the shape once drawn!
                         writeln("Starting fill");
                         bool isFilled = false;
 
@@ -206,6 +216,7 @@ class SDLApp{
                         writeln("Fill ended");
 
                     } else if (e.key.keysym.sym == SDLK_s) {
+                        /// This is where we draw the shape when prompted!
                         writeln("Drawing shape");
                         Shape sh = new Shape();
                         sh.drawShape(&imgSurface, brushSize, red, green, blue);
@@ -213,59 +224,65 @@ class SDLApp{
                 }
             }
 
-            // Blit the surace (i.e. update the window with another surfaces pixels
-            //                       by copying those pixels onto the window).
+            /// Blit the surace (i.e. update the window with another surfaces pixels
+            ///                       by copying those pixels onto the window).
             SDL_BlitSurface(imgSurface.getSurface(),null,SDL_GetWindowSurface(window),null);
-            // Update the window surface
+            /// Update the window surface
             SDL_UpdateWindowSurface(window);
-            // Delay for 16 milliseconds
-            // Otherwise the program refreshes too quickly
+            /// Delay for 16 milliseconds
+            /// Otherwise the program refreshes too quickly
             SDL_Delay(16);
         }
 
-        // Destroy our window
+        /// Destroy our window
         SDL_DestroyWindow(window);
 
     }
 }
 
-///Test: Checks for the surface to be initialized to black RGB values or 0,0,0
+/**
+Test: Checks for the surface to be initialized to black RGB values or 0,0,0
+*/
 @("Initialization test")
 unittest{
     SDLInit app = new SDLInit();
     Surface s = new Surface(0,640,480,32,0,0,0,0);
-    //parse values of new data struct
+    /// Parse values of new data struct
     assert(	s.PixelAt(1,1)[0] == 0 &&
     s.PixelAt(1,1)[1] == 0 &&
     s.PixelAt(1,1)[2] == 0, "error bgr value at x,y is wrong!");
 }
 
-///Test: Checks for the surface to be initialized to black, change the pixel color of 1,1 to blue
+/**
+Test: Checks for the surface to be initialized to black, change the pixel color of 1,1 to blue
+*/
 @("Single Blue Pixel test")
 unittest{
     SDLInit app = new SDLInit();
     Surface s = new Surface(0,640,480,32,0,0,0,0);
     s.UpdateSurfacePixel(1,1, 255, 128, 32);
-    //parse values of new data struct
+    /// Parse values of new data struct
     assert(	s.PixelAt(1,1)[0] == 255 &&
     s.PixelAt(1,1)[1] == 128 &&
     s.PixelAt(1,1)[2] == 32, "error bgr value at x,y is wrong!");
 }
 
-///Test: Checks for the surface to be initialized to black, change the pixel color of 1,1 to blue, verify its blue,
-///change it to red, ensure that the color of 1,1 is now red
+/**
+Test: Checks for the surface to be initialized to black, change the pixel color of 1,1 to blue, verify its blue,
+change it to red, ensure that the color of 1,1 is now red
+*/
 @("Change a pixel test")
 unittest{
     SDLInit app = new SDLInit();
     Surface s = new Surface(0,640,480,32,0,0,0,0);
     s.UpdateSurfacePixel(1,1, 255, 128, 32);
-    //parse values of new data struct
+    /// Parse values of new data struct
     assert(	s.PixelAt(1,1)[0] == 255 &&
     s.PixelAt(1,1)[1] == 128 &&
     s.PixelAt(1,1)[2] == 32, "error bgr value at x,y is wrong!");
-    //change the color of the pixel and make sure the change takes
+    /// Change the color of the pixel and make sure the change takes
     s.UpdateSurfacePixel(1,1, 32, 128, 255);
-    //parse values of new data struct
+    /// Parse values of new data struct
     assert(	s.PixelAt(1,1)[0] == 32 &&
     s.PixelAt(1,1)[1] == 128 &&
     s.PixelAt(1,1)[2] == 255, "error bgr value at x,y is wrong!");
