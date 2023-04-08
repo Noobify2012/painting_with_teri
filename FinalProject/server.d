33eed5bf294763e6e793cf3c98e29fc0db6437be
anniepates <82011145+anniepates@users.noreply.github.com>
2023-04-08 05:04:03 -0400

    Commented files. Still need to finsih shapes. Does not run becasue I left in legacy code when I merged SDLApp with Matt since I don't understand why he got rid of run application

--- FinalProject/server.d
+++ FinalProject/server.d
@@ -8,7 +8,9 @@ import Packet : Packet;
 import test_addr;
 
 
-
+/**
+Set up second terminal to run server.
+*/
 void main(){
 
 	Address serverAddr = test_addr.find();
@@ -34,59 +36,59 @@ void main(){
 	// writeln("this is the family and socket stream: " ~ AddressFamily.INET, SocketType.STREAM);
 	scope(exit) listener.close();
 
-	// Set the hostname and port for the socket
+	/// Set the hostname and port for the socket
     string host = dumb[0];
-    ushort port = 50002;
-	// NOTE: It's possible the port number is in use if you are not able
-	//  	 to connect. Try another one.
+    ushort port = 50002;  // TODO: need to stop hardcoding this
+	/// NOTE: It's possible the port number is in use if you are not able
+	///  	 to connect. Try another one.
 
     listener.bind(new InternetAddress(host,port));
-    // Allow 4 connections to be queued up
+    /// Allow 4 connections to be queued up
     listener.listen(4);
 
-	// A SocketSet is equivalent to 'fd_set'
-	// https://linux.die.net/man/3/fd_set
-	// What SocketSet is used for, is to allow 
-	// 'multiplexing' of sockets -- or put another
-	// way, the ability for multiple clients
-	// to connect a socket to this single server
-	// socket.
+	/// A SocketSet is equivalent to 'fd_set'
+	/// https://linux.die.net/man/3/fd_set
+	/// What SocketSet is used for, is to allow 
+	/// 'multiplexing' of sockets -- or put another
+	/// way, the ability for multiple clients
+	/// to connect a socket to this single server
+	/// socket.
     auto readSet = new SocketSet();
     Socket[] connectedClientsList;
 
-    // Message buffer will be large enough to send/receive Packet.sizeof
+    /// Message buffer will be large enough to send/receive Packet.sizeof
     byte[Packet.sizeof] buffer;
 
     bool serverIsRunning=true;
 	// int userID = 1;
 
-    // Main application loop for the server
+    /// Main application loop for the server
 	writeln("Awaiting client connections");
     while(serverIsRunning){
-		// Clear the readSet
+		/// Clear the readSet
         readSet.reset();
-		// Add the server
+		/// Add the server
         readSet.add(listener);
         foreach(client ; connectedClientsList){
             readSet.add(client);
         }
         
-        // Handle each clients message
+        /// Handle each clients message
         if(Socket.select(readSet, null, null)){
             foreach(client; connectedClientsList){
-				// Check to ensure that the client
-				// is in the readSet before receving
-				// a message from the client.
+				/// Check to ensure that the client
+				/// is in the readSet before receving
+				/// a message from the client.
                 if(readSet.isSet(client)){
-					// Server effectively is blocked
-					// until a message is received here.
-					// When the message is received, then
-					// we send that message from the 
-					// server to the client
+					/// Server effectively is blocked
+					/// until a message is received here.
+					/// When the message is received, then
+					/// we send that message from the 
+					/// server to the client
                     auto got = client.receive(buffer);
 					
-					// Setup a packet to echo back
-					// to the client
+					/// Setup a packet to echo back
+					/// to the client
 					Packet p;
 				    p.user 	= "connecting...";
 					byte[4] field1 = buffer[16 .. 20].dup;
@@ -105,21 +107,21 @@ void main(){
 					p.g = f4;
 					p.b = f5;
 					
-					// Send raw bytes from packet,
+					/// Send raw bytes from packet,
                     client.send(p.GetPacketAsBytes());
 					writeln("readset is set for client");
                 }
             }
-			// The listener is ready to read
-			// Client wants to connect so we accept here.
+			/// The listener is ready to read
+			/// Client wants to connect so we accept here.
 			if(readSet.isSet(listener)){
 				auto newSocket = listener.accept();
-				// Based on how our client is setup,
-				// we need to send them an 'acceptance'
-				// message, so that the client can
-				// proceed forward.
+				/// Based on how our client is setup,
+				/// we need to send them an 'acceptance'
+				/// message, so that the client can
+				/// proceed forward.
 				newSocket.send("Welcome from server, you are now in our connectedClientsList");
-				// Add a new client to the list
+				/// Add a new client to the list
 				connectedClientsList ~= newSocket;
 				writeln("> client",connectedClientsList.length," added to connectedClientsList");
 				writeln("readset is set for listener");
@@ -129,6 +131,6 @@ void main(){
 	}
 }
 
-//TODO: Method for sending packets to every other client
+// TODO: Method for sending packets to every other client
 // get packet from user
 // loop through all other users and send packet(think broadcast)
